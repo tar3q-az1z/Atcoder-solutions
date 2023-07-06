49c08cd72268619ac88a96600c54cfde723a337e
aziz <mdtareqa90@gmail.com>
2023-07-06 11:17:04 +0600

    abc308_a - New Scheme - br + st class

--- P308A.java
+++ P308A.java
@@ -1,4 +1,9 @@
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 /*
  * author: Mars_Coder
  * date: 06-07-2023
@@ -6,11 +11,12 @@ import java.util.Scanner;
  * note: while submitting, rename class name to Main if not!
  */
 public class P308A{
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		int[] a = new int[8];
 		for(int i = 0; i < 8; ++i){
-			a[i] = sc.nextInt();
+			a[i] = Integer.parseInt(st.nextToken());
 			if(i > 0 && a[i] < a[i - 1]){
 				System.out.println("No");
 				return;
