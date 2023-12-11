58cb27404dfe0f22d3fe28bee9442cbc01fa40e7
aziz <mdtareqa90@gmail.com>
2023-12-11 23:47:08 +0600

    Middle Letter

---
+++ Atcoder Beginner Contest(ABC)/abc266_a.cpp
@@ -0,0 +1,20 @@
+// Author : Mars_Coder
+// Date   : //2022
+
+#define _USE_MATH_DEFINES
+#include<bits/stdc++.h>
+using namespace std;
+#define PI    3.141592653589793238462643383279502884L
+#define ln "\n" // no flush, oppos of endl
+#define _flush endl
+#define stop_sync ios::sync_with_stdio(false) // not to use c-stye io
+#define untie_ios cin.tie(nullptr) // no flush
+#define FOR(a, b, c) for(auto i = a; i < b; i += c)
+#define lli long long int
+
+int main(void){
+	string s;
+	cin >> s;
+	cout << s[s.size() / 2] << ln;
+	return 0;
+}
