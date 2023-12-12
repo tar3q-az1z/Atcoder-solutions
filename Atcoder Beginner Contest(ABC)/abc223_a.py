05bcdc55c79ceec434059bf0716ba6aa66a13a6e
aziz <mdtareqa90@gmail.com>
2023-12-12 18:31:22 +0600

    Exact Price

---
+++ Atcoder Beginner Contest(ABC)/abc223_a.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  2 12:00:47 2023
+
+@author: Mars_Coder
+"""
+x = int(input())
+if x % 100 == 0 and x != 0:
+    print("Yes")
+else:
+    print("No")
