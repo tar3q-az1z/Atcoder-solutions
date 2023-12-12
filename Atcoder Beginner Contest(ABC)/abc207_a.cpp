9c41781da7384e150486bc2df17b68ffd55386fc
aziz <mdtareqa90@gmail.com>
2023-12-12 18:46:46 +0600

    Repression

---
+++ Atcoder Beginner Contest(ABC)/abc207_a.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<cstring>
+#include<stack>
+#include<queue>
+#include<cstdio>
+#include<set>
+#include<unordered_set>
+#include<cmath>
+#include<algorithm>
+#include<functional>
+#include<utility>
+#include<cstdlib>
+using namespace std;
+typedef long long int lli;
+typedef size_t idx;
+#define vi vector<int>
+#define pb(n) push_back(n)
+#define ln "\n"
+#define sp ends
+#define newline cout << ln
+#define fastios ios_base::sync_with_stdio(false); cin.tie(0)
+
+
+int main(){
+	int v[3];
+	cin >> v[0] >> v[1] >> v[2];
+	sort(v, v + 3);
+	cout << v[1] + v[2] << ln; 
+	
+	return 0;
+}
