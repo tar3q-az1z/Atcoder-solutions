96f85dcaa4698181fb137dfa4ffcc1b0f59ac7e4
aziz <mdtareqa90@gmail.com>
2023-12-12 18:35:47 +0600

    Atcoder Quiz 2

---
+++ Atcoder Beginner Contest(ABC)/abc219_a.cpp
@@ -0,0 +1,51 @@
+#define _USE_MATH_DEFINES
+
+#include<iostream>
+#include<vector>
+#include<string>
+#include<cstring>
+#include<stack>
+#include<queue>
+#include<cstdio>
+#include<set>
+#include<unordered_set>
+#include<cmath>
+#include<algorithm>
+#include<functional>
+#include<iomanip>
+#include<utility>
+#include<cstdlib>
+using namespace std;
+typedef long long int lli;
+typedef size_t idx;
+#define vi vector<int>
+#define vl vector<lli>
+#define pb(n) push_back(n)
+#define ln "\n"
+#define sp ends
+#define IT(x) x.begin(), x.end()
+#define loop(start, end, incre) for(int i = start; i < end; i += incre)
+#define newline cout << ln
+const int MOD = 1000000007;
+#define precision(n) cout << fixed << setprecision(n) // n places after dec.point
+#define fastios ios_base::sync_with_stdio(false); cin.tie(0)
+
+
+int main(){
+
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+
+	int a;
+	cin >> a;
+	if(a >= 90) cout << "expert\n";
+	else if(a >= 0 && a < 40) cout << 40 - a << ln;
+	else if(a >= 40 && a < 70){
+		cout << 70 - a << ln;
+	}else if( a >= 70 && a < 90){
+		cout << 90 - a << ln;
+	}
+	return 0;
+}
