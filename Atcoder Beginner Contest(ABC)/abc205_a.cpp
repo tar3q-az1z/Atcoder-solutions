9d06eed06608ca569c705d52953aa7d057e19a81
aziz <mdtareqa90@gmail.com>
2023-12-12 18:53:40 +0600

    kcal

---
+++ Atcoder Beginner Contest(ABC)/abc205_a.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<cstring>
+#include<cstdio>
+#include<cmath>
+#include<algorithm>
+#include<functional>
+#include<utility> // pair
+#include<cstdlib> // EXIT_SUCCESS,...
+using namespace std;
+typedef long long int lli;
+typedef size_t idx;
+int main(){
+	int a, b;
+	cin >> a >> b;
+//	cout << (a * b/ 100)  << endl;
+	printf("%lf\n",(a*b)/100.0);
+	
+	return 0;
+}
